663d0bca783daec660832ab9c36d9382d7238655
Ibrahim Sikiru (IB) <ibrahimsikiru@cmail.carleton.ca>
2015-06-13 17:54:23 -0400

    finished scrum

--- scrum/scrum.js
+++ scrum/scrum.js
@@ -20,52 +20,77 @@ rl
 		rl.prompt();
 	})
 	.on('close', function() {
-	  console.log('closed properly');
-	  process.exit(0);
+		db.close(function(err) {
+			if (err) {
+				console.log("database not closed properly");
+				console.log("exiting with error code: 44");
+				process.exit(44);
+			}
+			else {
+				console.log('bye!!');
+				process.exit(0);
+			}
+		})
 	});
 
 function eval(line) {
-	input = line.match(/\w+|"(?:\\"|[^"\s])+"/g);
+	input = line.replace(/\s\s+/g, ' ').trim().split(" ");//match(/\w+|"(?:\\"|[^"\s])+"/g);
 	if (input) {
-		multi_eval(input);
+		multi_eval(input, function() {
+			rl.prompt();
+		});
 	}
-	rl.prompt();
+	else {
+		rl.prompt();
+	}
 }
 
-function multi_eval(commands) {
+function multi_eval(commands, finished) {
 	var first = commands.shift();
 	switch (first) {
+		case '':
+			if (finished)
+				finished();
+			break;
 		case 'q':
 		case 'quit':
-			quit_command(commands);
+			quit_command(commands, finished);
 			break;
 		case 'h':
 		case 'help':
-			help_command(commands);
+			help_command(commands, finished);
 			break;
 		case 'u':
 		case 'user':
-			user_command(commands);
+			user_command(commands, finished);
+			break;
+		case 't':
+		case 'task':
+			task_command(commands, finished);
 			break;
 		default:
-			unknown_command_handler();
+			unknown_command_handler(finished);
 	}
 }
 
-function unknown_command_handler() {
+function unknown_command_handler(finished) {
 	console.log("huh???");
+	if (finished)
+		finished();
 }
 
-function show_usage_information() {
+function show_usage_information(finished) {
 	console.log("enter 'command -h|--help|help' to get usage");
 	console.log("information about a specific command");
 	console.log("supported commands are:");
 	console.log("[u]ser");
 	console.log("[h]elp");
 	console.log("[q]uit");
+	if (finished)
+		finished();
 }
 
-function quit_command(args) {
+function quit_command(args, finished) {
 	var first = args === undefined || args.length == 0 ? '' : args.shift();
 	switch (first) {
 		case '':
@@ -75,37 +100,45 @@ function quit_command(args) {
 		case '--help':
 		case 'help':
 			console.log("help info: quit");
+			if (finished)
+				finished();
 			break;
 		default:
-			unknown_command_handler();
+			unknown_command_handler(finished);
 	}
 }
 
-function help_command(args) {
+function help_command(args, finished) {
 	var first = args === undefined || args.length == 0 ? '' : args.shift();
 	switch (first) {
 		case '':
-			show_usage_information();
+			show_usage_information(finished);
 			break;
 		case 'h':
 		case '--help':
 		case 'help':
 			console.log("help info: help");
+			if (finished)
+				finished();
 			break;
 		default:
-			unknown_command_handler();
+			unknown_command_handler(finished);
 	}
 }
 
-function user_command(args) {
+function user_command(args, finished) {
 	var first = args === undefined || args.length == 0 ? '' : args.shift();
 	switch (first) {
 		case '':
+			if (finished)
+				finished();
 			break;
 		case '-h':
 		case '--help':
 		case 'help':
 			console.log("help info: user");
+			if (finished)
+				finished();
 			break;
 		case '-l':
 		case '--list':
@@ -118,9 +151,59 @@ function user_command(args) {
 			    	var fullname = user.full_name || '';
 			    	console.log(user.user_name + " - " + fullname);
 			    }
-			});
+			}, finished);
 			break;
 		default:
-			unknown_command_handler();
+			unknown_command_handler(finished);
+	}
+}
+
+function task_command(args, finished) {
+	var first = args === undefined || args.length == 0 ? '' : args.shift();
+	switch (first) {
+		case '':
+			if (finished)
+				finished();
+			break;
+		case '-h':
+		case '--help':
+		case 'help':
+			console.log("help info: task");
+			if (finished)
+				finished();
+			break;
+		case '-l':
+		case '--list':
+		case 'list':
+			db.each("select task_id, task_summary, user_name from tasks", function(err, task) {
+			    if (err) {
+			    	console.log("error getting information from database");
+			    }
+			    else {
+			    	var username = task.user_name || '';
+			    	console.log(task.task_id + " - " + task.task_summary + " - " + username);
+			    }
+			}, finished);
+			break;
+		case '-a':
+		case '--add':
+		case 'add':
+			var stmt = db.prepare("insert into tasks (task_summary) values (?)");
+			stmt.run(args.join(" "));
+			stmt.finalize();
+			if (finished)
+				finished();
+			break;
+		case '-as':
+		case '--assign':
+		case 'assign':
+			var stmt = db.prepare("update tasks set user_name = ? where (task_id = ?)");
+			stmt.run(args);
+			stmt.finalize();
+			if (finished)
+				finished();
+			break;
+		default:
+			unknown_command_handler(finished);
 	}
 }
