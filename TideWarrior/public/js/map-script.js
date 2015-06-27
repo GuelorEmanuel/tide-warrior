ee6bbd421e50b8b4f08fa44281bf44de1d57999f
christian michael <chukwudimichael@cmail.carleton.ca>
2015-06-27 18:18:48 -0400

    Merged with master

--- TideWarrior/public/js/map-script.js
+++ TideWarrior/public/js/map-script.js
@@ -2,6 +2,23 @@ var drawMap = function (accessToken, map_center, pin_location) {
 	L.mapbox.accessToken = accessToken;
 	var map = L.mapbox.map('map', 'chukzzy.mdcmd242') .setView([map_center.latitude, map_center.longitude], map_center.zoom);
 
+	var directions = L.mapbox.directions();
+
+	var directionsLayer = L.mapbox.directions.layer(directions)
+	    .addTo(map);
+
+	var directionsInputControl = L.mapbox.directions.inputControl('inputs', directions)
+	    .addTo(map);
+
+	var directionsErrorsControl = L.mapbox.directions.errorsControl('errors', directions)
+	    .addTo(map);
+
+	var directionsRoutesControl = L.mapbox.directions.routesControl('routes', directions)
+	    .addTo(map);
+
+	var directionsInstructionsControl = L.mapbox.directions.instructionsControl('instructions', directions)
+		.addTo(map);
+
 	if (pin_location) {
 		var marker_properties = {
 			'marker-size': 'large',
