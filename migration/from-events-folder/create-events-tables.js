a093c64c9183f8f217fe7e3f7559aed17ee86603
Ibrahim Sikiru <ib@iamtheib.me>
2015-07-11 03:21:32 -0400

    Added migration script to create tables for and populate events in the database

--- migration/from-events-folder/create-events-tables.js
+++ migration/from-events-folder/create-events-tables.js
@@ -30,18 +30,15 @@ db
 	 });
 
 var afterDatabaseConnection = function () {
-	// the tables that store places information
-	var categoriesTb = 'places_categories',
-		placesTb = 'places',
+	// the tables that store events information
+	var categoriesTb = 'events_categories',
 		eventsTb = 'events',
 		geolocationsTb = 'geolocations';
 
 	// the order the tables are dropped matters
-	// this also drops the events table as it gets pretty
-	// useless after this
+	// this script should not drop the geolocations table
 	var dropTbsStatement = db.prepare("DROP TABLE IF EXISTS " +
-							  placesTb + ", " + eventsTb + ", " +
-							  categoriesTb + ", " + geolocationsTb);
+							  eventsTb + ", " + categoriesTb);
 
 	var createCategoriesTbStatement = db.prepare("CREATE TABLE " + categoriesTb +
 										 "( " +
@@ -51,27 +48,21 @@ var afterDatabaseConnection = function () {
 										 "UNIQUE (category_name) " +
 										 ")");
 
-	var createGeolocationsTbStatement = db.prepare("CREATE TABLE " + geolocationsTb +
-										   "( " +
-										   "location_id INT NOT NULL AUTO_INCREMENT, " +
-										   "location_points POINT NOT NULL, " +
-										   "location_address VARCHAR(200), " +
-										   "PRIMARY KEY (location_id), " +
-										   "UNIQUE (location_points) " +
-										   ")");
-
-	var createPlacesTbStatement = db.prepare("CREATE TABLE " + placesTb +
+	var createEventsTbStatement = db.prepare("CREATE TABLE " + eventsTb +
 									 "( " +
-								     "place_id INT NOT NULL AUTO_INCREMENT, " +
-								     "place_category INT NOT NULL, " +
-								     "place_location INT NOT NULL, " +
-								     "place_name VARCHAR(100), " +
-								     "place_tags TEXT, " +
-								     "PRIMARY KEY (place_id), " +
-								     "FOREIGN KEY (place_category) " +
+								     "event_id INT NOT NULL AUTO_INCREMENT, " +
+								     "event_category INT NOT NULL, " +
+								     "event_location INT NOT NULL, " +
+								     "event_name VARCHAR(100), " +
+								     "event_tags TEXT, " +
+								     "event_date DATE, " +
+								     "event_time TIME, " +
+								     "event_description VARCHAR(150), " +
+								     "PRIMARY KEY (event_id), " +
+								     "FOREIGN KEY (event_category) " +
 								     "REFERENCES " + categoriesTb + " (category_id) " +
 								     "ON UPDATE CASCADE ON DELETE CASCADE, " +
-								     "FOREIGN KEY (place_location) " +
+								     "FOREIGN KEY (event_location) " +
 								     "REFERENCES " + geolocationsTb + " (location_id) " +
 								     "ON UPDATE CASCADE ON DELETE CASCADE " +
 									 ")");
@@ -79,14 +70,9 @@ var afterDatabaseConnection = function () {
 	var insertCategoryStatement = db.prepare("INSERT INTO " + categoriesTb + " SET " +
 									 "category_name = ?");
 
-	var insertLocationStatement = db.prepare("INSERT INTO " + geolocationsTb + " SET " +
-									 "location_points = PointFromText(:geometry), " +
-									 "location_address = :addr ON DUPLICATE KEY UPDATE " +
-									 "location_address = :addr");
-
-	var insertPlacesStatement = db.prepare("INSERT INTO " + placesTb + "(place_category, " +
-								   "place_location, place_name, place_tags) SELECT " + categoriesTb +
-								   ".category_id, " + geolocationsTb + ".location_id, ?, ? FROM " +
+	var insertEventsStatement = db.prepare("INSERT INTO " + eventsTb + "(event_category, " +
+								   "event_location, event_name, event_tags, event_date) SELECT " + categoriesTb +
+								   ".category_id, " + geolocationsTb + ".location_id, ?, ?, ? FROM " +
 								   categoriesTb + ", " + geolocationsTb + " WHERE " + categoriesTb +
 								   ".category_name = ? AND " + geolocationsTb + ".location_points = " +
 								   "PointFromText(?)");
@@ -115,13 +101,13 @@ var afterDatabaseConnection = function () {
 	};
 
 	var insertData = function () {
-		var places_dir = path.resolve(process.env.PWD, '../../locations/places');
-		console.log(scriptPrefix + "Inserting the data from the json files in " + places_dir);
-		fs.readdir(places_dir, function(err, files) {
+		var events_dir = path.resolve(process.env.PWD, '../../events');
+		console.log(scriptPrefix + "Inserting the data from the json files in " + events_dir);
+		fs.readdir(events_dir, function(err, files) {
 		 	if (err) {
 		 		errorOccured = true;
-		 		console.log(scriptPrefix + "Error opening 'places' directory");
-		 		console.log(scriptPrefix + "Make sure the places json files are in " + places_dir);
+		 		console.log(scriptPrefix + "Error opening 'events' directory");
+		 		console.log(scriptPrefix + "Make sure the events json files are in " + events_dir);
 		 		console.log(scriptPrefix + "Error occured, disconnecting from database server");
 				db.end();
 		 	}
@@ -141,40 +127,35 @@ var afterDatabaseConnection = function () {
 
 			 	files.forEach(function(file) {
 			 		++semaphore;
-			 		var filePath = path.resolve(places_dir, file);
+			 		var filePath = path.resolve(events_dir, file);
 			 		fs.readFile(filePath,'utf8',function(err, data) {
 			 			--semaphore;
 			 			if (!err) {
 			 				var categoryName = path.basename(filePath, '.json').split("_").join(" ");
 			 				var jsonObj = JSON.parse(data);
 
-			 				var insertEachPlace = function () {
-				 				jsonObj.results.forEach(function(info) {
-					 					var placeLat = info.geometry.location.lat;
-					 					var placeLng = info.geometry.location.lng;
-					 					var placeName = info.name;
-					 					var placeTags = info.types.toString();
-					 					var placeAddress = info.vicinity;
+			 				var insertEachEvent = function () {
+				 				jsonObj.randomEvents.forEach(function(info) {
+					 					var eventLat = info.geometry.location.lat;
+					 					var eventLng = info.geometry.location.lng;
+					 					var eventName = info.name;
+					 					var dateObj = new Date(info.date);
+					 					var eventDate = dateObj.getFullYear() + "-" +
+					 									(dateObj.getMonth() + 1) + "-" +
+					 									dateObj.getDate();
+					 					var eventTags = categoryName+",party,fun";
 
-					 					var insertThisPlace = function () {
-					 						runQuery(insertPlacesStatement, checkLastQuery, [
-					 								placeName,
-					 								placeTags,
-					 								categoryName,
-					 								"POINT(" + placeLat + " " + placeLng + ")"
-					 							]);
-					 					};
-
-					 					runQuery(insertLocationStatement, insertThisPlace,
-					 							 {
-					 							 	geometry: "POINT(" + placeLat +
-					 							 			  " " + placeLng + ")",
-					 							 	addr: placeAddress
-					 							 });
+				 						runQuery(insertEventsStatement, checkLastQuery, [
+				 								eventName,
+				 								eventTags,
+				 								eventDate,
+				 								categoryName,
+				 								"POINT(" + eventLat + " " + eventLng + ")"
+				 							]);
 				 				});
 			 				};
 
-			 				runQuery(insertCategoryStatement, insertEachPlace, [categoryName]);
+			 				runQuery(insertCategoryStatement, insertEachEvent, [categoryName]);
 			 			}
 			 		});
 			 	});
@@ -182,33 +163,27 @@ var afterDatabaseConnection = function () {
 		});
 	};
 
-	var createPlacesTbCallback = function () {
-		console.log(scriptPrefix + "Created new Table '" + placesTb + "'");
-		// insert the data from the json files in locations/places
+	var createEventsTbCallback = function () {
+		console.log(scriptPrefix + "Created new Table '" + eventsTb + "'");
+		// insert the data from the json files in events
 		// folder into the database
 		insertData();
 	};
 
-	var createGeolocationsTbCallback = function () {
-		console.log(scriptPrefix + "Created new Table '" + geolocationsTb + "'");
-		// create new table that will store the actual places information
-		runQuery(createPlacesTbStatement, createPlacesTbCallback);
-	};
-
 	var createCategoriesTbCallback = function () {
 		console.log(scriptPrefix + "Created new Table '" + categoriesTb + "'");
-		// create new table that will store the geolocation information
-		runQuery(createGeolocationsTbStatement, createGeolocationsTbCallback);
+		// create new table that will store the main events information
+		runQuery(createEventsTbStatement, createEventsTbCallback);
 	};
 
 	var dropTbsCallback = function () {
-		console.log(scriptPrefix + "Dropped Existing Tables '" + placesTb +
-					"' '" + categoriesTb + "' '" + geolocationsTb + "'");
-		// create new table that will store the places categories information
+		console.log(scriptPrefix + "Dropped Existing Tables '" + eventsTb +
+					"' '" + categoriesTb + "'");
+		// create new table that will store the events categories information
 		runQuery(createCategoriesTbStatement, createCategoriesTbCallback);
 	};
 
-	// drop all existing tables that store places information
+	// drop all existing tables that store events information
 	runQuery(dropTbsStatement, dropTbsCallback);
 
 	if (errorOccured) {
